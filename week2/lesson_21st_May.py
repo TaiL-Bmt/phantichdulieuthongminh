131a0a7675df025c5074d381918b447b9f7f3f5f
TaiL <trongtaile1990@gmail.com>
2022-05-23 22:41:02 +0700

    exercises week2 21st May

---
+++ week2/lesson_21st_May.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Read dataset *.csv file
+df = pd.read_csv('./data/data-vn.csv')
+
+# Do some clean data
+df['income'] = df['income'].replace({',':'.'}, regex=True).astype(float)
+df['outcome'] = df['outcome'].replace({',':'.'}, regex=True).astype(float)
+df['food_expenditure'] = df['food_expenditure'].replace({',':'.'}, regex=True).astype(float)
+print(df.columns)
+
+# Graph scatter income/outcome
+if 0:
+    fig,ax = plt.subplots(1, 1)
+    ax.scatter(df['income'], df['outcome'], color='b')
+    ax.set_xlabel('income')
+    ax.set_ylabel('outcome')
+    ax.set_title('income/outcome scatter figure')
+    plt.show()
+
+# Histogram graphs of income rural/urban
+if 0:
+    fig,ax = plt.subplots(1, 2)
+    bins_number = 25
+    # Rural
+    df_rural= df.loc[df['area'] == 'rural']
+    ax[0].hist(df_rural['income'], bins=bins_number, color='b')
+    ax[0].set_title('histogram of rural income')
+    # Urban
+    df_urban= df.loc[df['area'] == 'urban']
+    ax[1].hist(df_urban['income'], bins=bins_number, color='r')
+    ax[1].set_title('histogram of urban income')
+
+    plt.show()
+
+# Contour graph of income/outcome
+if 1:
+    fig,ax = plt.subplots(1, 1)
+    x = df['income']
+    y = df['outcome']
+    ax.set_title('seaborn graph')
+    sns.kdeplot(df['income'], df['outcome'], ax=ax)
+    #sns.rugplot(df['income'], color='g', ax=ax)
+    #sns.rugplot(df['outcome'], vertical=True, ax=ax, color='k')
+    plt.show()
+
