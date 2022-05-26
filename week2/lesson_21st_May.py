0951fb265dcf4a38494b0383565875e283291d71
TaiL <trongtaile1990@gmail.com>
2022-05-26 21:56:30 +0700

    Update edgecolor

--- week2/lesson_21st_May.py
+++ week2/lesson_21st_May.py
@@ -15,7 +15,7 @@ print(df.columns)
 # Graph scatter income/outcome
 if 0:
     fig,ax = plt.subplots(1, 1)
-    ax.scatter(df['income'], df['outcome'], color='b')
+    ax.scatter(df['income'], df['outcome'], color='b', edgecolor='grey')
     ax.set_xlabel('income')
     ax.set_ylabel('outcome')
     ax.set_title('income/outcome scatter figure')
@@ -27,11 +27,11 @@ if 0:
     bins_number = 25
     # Rural
     df_rural= df.loc[df['area'] == 'rural']
-    ax[0].hist(df_rural['income'], bins=bins_number, color='b')
+    ax[0].hist(df_rural['income'], bins=bins_number, color='b', edgecolor='grey')
     ax[0].set_title('histogram of rural income')
     # Urban
     df_urban= df.loc[df['area'] == 'urban']
-    ax[1].hist(df_urban['income'], bins=bins_number, color='r')
+    ax[1].hist(df_urban['income'], bins=bins_number, color='green', edgecolor='grey')
     ax[1].set_title('histogram of urban income')
 
     plt.show()
@@ -55,7 +55,8 @@ if 1:
     df_rural= df.loc[df['area'] == 'rural']
     # Urban
     df_urban= df.loc[df['area'] == 'urban']
-    ax.hist([df_rural['income'], df_urban['income']], bins=bins_number, color=['b', 'g'], edgecolor='black', stacked=True)
+    ax.hist([df_rural['income'], df_urban['income']], bins=bins_number, color=['b', 'g'], edgecolor='grey', stacked=True)
+    ax.set_title('Stacked histogram urban/rural income')
 
     plt.show()
 
